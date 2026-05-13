b155d8fb1344bc56bfe2d0957c667c31bb98282a
Michael Papadopoulos <micpap25@gmail.com>
2026-05-13 14:23:00 -0400

    further experiments as well as bugfixes

--- src/classical_solve_lin_program.py
+++ src/classical_solve_lin_program.py
@@ -1,42 +1,10 @@
 import json
 import numpy as np
 
-# Take in an LP and compute the condition number of A
-# Ignoring the bound constraints
-def condition_number_no_bounds(f_name: str = "linear_approx.json", 
-                                verbose: bool = False) -> float:
-    with open(f_name, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    variables = data["variables"]
-    constraints = data["constraints"]
-    n_variables = len(variables)
-    n_constraints = len(constraints)
-
-    # Assign the variables to rows
-
-    var_name_to_row_index = {}
-    for i, (var_name, _) in enumerate(variables.items(), start=0):
-        var_name_to_row_index[var_name] = i
-
-    # Create the matrix of non-bound constraints
-    constraint_matrix = np.zeros((n_constraints, n_variables))
-
-    for i, constraint_data in enumerate(constraints.values(), start=0):
-        assert len(constraint_data["body"]["quadratic"]) == 0
-        linear_data = constraint_data["body"]["linear"]
-        for lin_variable in linear_data:
-            j = var_name_to_row_index[lin_variable["var"]]
-            constraint_matrix[i, j] = lin_variable["coef"]
-
-    if verbose:
-        print(constraint_matrix)
-
-    return np.linalg.cond(constraint_matrix)
-
-# Take in an LP and compute the condition number of A
-def condition_number(f_name: str = "linear_approx.json",
-                        verbose: bool = False) -> float:
+# Take in an LP from a JSON and solve it
+# Use this to benchmark the linear approximation
+def solve_lp(f_name: str = "linear_approx.json",
+                verbose: bool = False) -> float:
     with open(f_name, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
@@ -89,5 +57,4 @@ def condition_number(f_name: str = "linear_approx.json",
     return np.linalg.cond(A)
 
 if __name__ == "__main__":
-    # print(condition_number(verbose=True))
-    print(condition_number_no_bounds(verbose=True))
+    print(solve_lp(verbose=True))
