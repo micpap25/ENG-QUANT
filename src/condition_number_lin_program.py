29c074b4e7536880d363815fb2472148136f5342
Michael Papadopoulos <micpap25@gmail.com>
2026-05-16 01:07:13 -0400

    Condition number for normal equation system

--- src/condition_number_lin_program.py
+++ src/condition_number_lin_program.py
@@ -1,6 +1,72 @@
 import json
 import numpy as np
 
+# Take in an LP and compute the condition number of a matrix
+# That should represent the system being solved in the QIPM
+# According to discussions on 4/30/2026
+# If the problem is min c^Tx  s.t. Ax = b, A'x >= d,  -x >= -u, x >= l
+# Then the matrix is A^TA + A'^TA' + 2I
+# This assumes the variables are bounded on both sides
+def condition_number_nes_basic(f_name: str = "linear_approx.json",
+                                verbose: bool = False) -> float:
+    with open(f_name, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    variables = data["variables"]
+    constraints = data["constraints"]
+    n_variables = len(variables)
+    n_constraints = len(constraints)
+
+    # Assign the variables to rows
+    var_name_to_row_index = {}
+    for i, var_name in enumerate(variables.keys(), start=0):
+        var_name_to_row_index[var_name] = i
+
+    # Get the number of equality constraints
+    n_equality_constraints = 0
+    for constraint in constraints.values():
+        if constraint["equality"]:
+            n_equality_constraints += 1
+    
+    n_inequality_constraints = n_constraints - n_equality_constraints
+
+    # Create the matrices of constraints
+    A = np.zeros((n_equality_constraints, n_variables))
+    A_bar = np.zeros((n_inequality_constraints, n_variables))
+
+    equality_pointer = 0
+    inequality_pointer = 0
+
+    for i, constraint_data in enumerate(constraints.values(), start=0):
+        assert len(constraint_data["body"]["quadratic"]) == 0
+        linear_data = constraint_data["body"]["linear"]
+        if constraint_data["equality"]:
+            for lin_variable in linear_data:
+                name = var_name_to_row_index[lin_variable["var"]]
+                A[equality_pointer, name] = lin_variable["coef"]
+            equality_pointer += 1
+        else:
+            if constraint_data["lower"] is None:
+                # it is a <= constraint, so flip it
+                for lin_variable in linear_data:
+                    name = var_name_to_row_index[lin_variable["var"]]
+                    A_bar[inequality_pointer, name] = -lin_variable["coef"]
+            else:
+                for lin_variable in linear_data:
+                    name = var_name_to_row_index[lin_variable["var"]]
+                    A_bar[inequality_pointer, name] = lin_variable["coef"]
+            inequality_pointer += 1
+
+
+    final_matrix = np.matmul(np.transpose(A), A) \
+                    + np.matmul(np.transpose(A_bar), A_bar) \
+                    + np.multiply(2, np.identity(n_variables))
+
+    if verbose:
+        print(final_matrix)
+
+    return np.linalg.cond(final_matrix)
+
 # Take in an LP and compute the condition number of A
 # Ignoring the bound constraints
 def condition_number_no_bounds(f_name: str = "linear_approx.json", 
@@ -90,4 +156,4 @@ def condition_number(f_name: str = "linear_approx.json",
 
 if __name__ == "__main__":
     # print(condition_number(verbose=True))
-    print(condition_number_no_bounds(verbose=True))
+    print(condition_number_nes_basic(verbose=True))
